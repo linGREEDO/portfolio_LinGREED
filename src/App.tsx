b410a2f05817199a2d6251e110268f99b94f5072
linGREEDO <diary.tahir21@knu.edu.iq>
2025-04-12 01:50:52 +0300

    feat: integrate SpeedInsights component into App for performance monitoring

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import { Analytics } from "@vercel/analytics/react";
-import { injectSpeedInsights } from '@vercel/speed-insights';
-
-injectSpeedInsights();
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const queryClient = new QueryClient();
 
@@ -25,6 +23,7 @@ const App = () => (
         </Routes>
       </BrowserRouter>
       <Analytics />
+      <SpeedInsights/>
     </TooltipProvider>
   </QueryClientProvider>
 );
